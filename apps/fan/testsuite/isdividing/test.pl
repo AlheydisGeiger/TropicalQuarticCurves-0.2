c620fc3b25efdf22c063b51507381cac01bf92d1
Alheydis Geiger <alheydisgeiger@web.de>
2025-02-24 16:07:39 +0100

    changes in compute_edges and related functions to fix the test for is_dividing

--- apps/fan/testsuite/isdividing/test.pl
+++ apps/fan/testsuite/isdividing/test.pl
@@ -3,7 +3,7 @@
 my $cells = new Array<Set<Int>>([[0,1,4],[0,2,4],[1,3,4],[2,4,5],[3,4,6],[4,5,9],[4,6,10],[4,7,10],[4,7,12],[4,8,12],[4,8,14],[4,9,14],[7,10,11],[7,11,12],[8,12,13],[8,13,14]]);
 my $trn = new DualSubdivisionOfQuartic(MAXIMAL_CELLS=>$cells);
 my $signs = new Vector<Int>([1,1,1,1,1,1,1,-1,1,1,1,1,1,-1,1]);
-compare_values("dividing", true, is_dividing($trn,$signs));
+compare_values("dividing", true, $trn->IS_DIVIDING($signs));
 
 my $cells2 = new Array<Set<Int>>([[0,1,2],[1,2,4],[1,3,4],[2,4,5],[3,6,7],[3,4,7],[4,7,8],[4,5,8],[5,8,9]]);
 my $trn2 = new SubdivisionOfPoints(POINTS=>[[1,0,0],[1,1,0],[1,0,1],[1,2,0],[1,1,1],[1,0,2],[1,3,0],[1,2,1],[1,1,2],[1,0,3]],MAXIMAL_CELLS=>$cells2);
