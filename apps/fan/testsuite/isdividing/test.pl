169c9dfda0db372979108e9114cd3222eb59ff71
Alheydis Geiger <alheydisgeiger@web.de>
2025-02-25 16:40:55 +0100

    neuer Test fuer isdividing

--- apps/fan/testsuite/isdividing/test.pl
+++ apps/fan/testsuite/isdividing/test.pl
@@ -9,3 +9,8 @@ my $cells2 = new Array<Set<Int>>([[0,1,2],[1,2,4],[1,3,4],[2,4,5],[3,6,7],[3,4,7
 my $trn2 = new SubdivisionOfPoints(POINTS=>[[1,0,0],[1,1,0],[1,0,1],[1,2,0],[1,1,1],[1,0,2],[1,3,0],[1,2,1],[1,1,2],[1,0,3]],MAXIMAL_CELLS=>$cells2);
 my $signs2 = new Vector<Int>([1,1,1,1,1,1,1,1,1,1]);
 compare_values("dividing2", true, is_dividing($trn2,$signs2));
+
+my $cells3 = new Array<Set<Int>>([[0,1,2,3]]);
+my $trn3 = new SubdivisionOfPoints(POINTS=>[[1,0,0,0],[1,1,0,0],[1,0,1,0],[1,0,0,1]],MAXIMAL_CELLS=>$cells3);
+my $signs3 = new Vector<Int>([1,1,1,1]);
+compare_expected_error { is_dividing($trn3,$signs3) } "dividing3";
